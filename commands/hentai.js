657094daa9d167306cf4d8ab7dd15c055e702fd7
TheCuriousGoose <justinlamaperez@gmail.com>
2021-05-26 20:01:41 +0200

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,12 +1,23 @@
 //hello pachu, if you ever see this. Don't worry this is for a different server, you can't actually use this on your server :)
 const Discord = require("discord.js");
-const { getPost, getImage } = require('random-reddit')
+const { getImage } = require('random-reddit')
 
-exports.run = async (client, message, args) => {
-
+exports.run = async(client, message, args) => {
     if(message.guild.id === '836707588640997456' || message.guild.id === '742732244673626234'){
-            const image = await getImage('hentai')
-            message.channel.send(image)
-    }
+        //if(message.member.id === '659690575859154955') return;
+        if(args[0]){
+            const image = await getImage(args[0])
+            message.channel.send(image).catch(e => message.channel.send('Something went wrong.'))
 
+
+        }else{
+            var subslist = ['hentai','rule34']
+            var subreddits = subslist[Math.floor(Math.random() * subslist.length)];
+            
+            const image = await getImage(subreddits)
+            message.channel.send(image).catch(e => message.channel.send('Something went wrong.'))
+        }
+    }
 }
+
+
